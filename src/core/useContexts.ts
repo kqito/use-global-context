0406534a5ada5d18bcf893e7acd98191907ee04e
kqito <29191111+kqito@users.noreply.github.com>
2020-09-22 20:32:25 +0900

    Add some comments

--- src/core/useContexts.ts
+++ src/core/useContexts.ts
@@ -1,6 +1,8 @@
-import React, { useContext, useLayoutEffect, useRef } from 'react';
+import React, { useContext, useLayoutEffect, useRef, useState } from 'react';
 import { HooksContextValues } from './types';
 
+// The useSelector logic is based on the following repository.
+// https://github.com/dai-shi/use-context-selector (MIT LICENSE)
 const createUseSelector = <State>(context: React.Context<any>) => {
   const defaultSelector = (state: State) => state;
 
@@ -11,39 +13,35 @@ const createUseSelector = <State>(context: React.Context<any>) => {
   function useSelector<SelectedState>(
     selector?: (state: State) => SelectedState
   ) {
-    const { eventListener } = context;
     const value = useContext(context);
-
-    const [, update] = React.useReducer((c) => c + 1, 0);
-    const callback = selector || defaultSelector;
-    const selectedValue = callback(value);
-    const state = useRef<{
+    const [, update] = useState<State>();
+    const prev = useRef<{
       value: any;
-      selector: any;
       selectedValue: any;
     } | null>(null);
 
+    const { eventListener } = context;
+    const callback = selector || defaultSelector;
+    const selectedValue = callback(value);
+
     useLayoutEffect(() => {
-      state.current = { value, selector: callback, selectedValue };
+      prev.current = { value, selectedValue };
     });
 
     useLayoutEffect(() => {
       const refresh = (nextValue: State) => {
-        if (!state.current) {
+        if (!prev.current) {
           return;
         }
 
         if (
-          state.current.value === nextValue ||
-          Object.is(
-            state.current.selectedValue,
-            state.current.selector(nextValue)
-          )
+          prev.current.value === nextValue ||
+          Object.is(prev.current.selectedValue, callback(nextValue))
         ) {
           return;
         }
 
-        update();
+        update(nextValue);
       };
 
       eventListener?.push(refresh);
