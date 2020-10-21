2ffafb61f60dbb0da0879cd8c7a2ed65d22b5193
kqito <29191111+kqito@users.noreply.github.com>
2020-10-21 18:29:41 +0900

    Fix isBrowser condition

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,6 +1,8 @@
 export const isBrowser =
-  (typeof window !== 'undefined' &&
-    typeof window.document !== 'undefined' &&
-    typeof window.document.createElement !== 'undefined') ||
-  (process.env.NODE_ENV !== 'production' &&
-    process.env.TEST_MODE === 'Browser');
+  typeof window !== 'undefined' &&
+  typeof window.document !== 'undefined' &&
+  typeof window.document.createElement !== 'undefined' &&
+  !(
+    process.env.NODE_ENV !== 'production' &&
+    process.env.USE_GLOBAL_CONTEXT_TEST_MODE === 'server'
+  );
