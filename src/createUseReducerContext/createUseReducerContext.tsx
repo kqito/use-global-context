71cb63cc7777070f153600dbaa2335cd13754311
kqito <29191111+kqito@users.noreply.github.com>
2020-11-08 16:21:30 +0900

    Rename from displayName to partial

--- src/createUseReducerContext/createUseReducerContext.tsx
+++ src/createUseReducerContext/createUseReducerContext.tsx
@@ -10,7 +10,7 @@ import {
 import { UseSelector } from '../core/useSelector';
 
 export type UseReducerContextSource = {
-  [displayName: string]: {
+  [partial: string]: {
     reducer: AnyReducer;
     initialState: any;
     initializer?: any;
@@ -23,11 +23,6 @@ export type UseReducerContextSource = {
  * respectively, to prevent unnecessary rendering.
  */
 export const createUseReducerContext = <T extends UseReducerContextSource>(
-  /**
-   *  Object's value is passed as an argument to useReducer.
-   *  Also, the object's key is set to the context's displayname.
-   *  *@see* https://reactjs.org/docs/context.html#contextdisplayname
-   */
   contextSource: T
 ): [UseSelector<UseReducerStore<T>>, React.FC<ContextProvider<State<T>>>] => {
   const { useGlobalContext, contextProvider } = createContext(contextSource);
--- src/createUseStateContext/createUseStateContext.tsx
+++ src/createUseStateContext/createUseStateContext.tsx
@@ -3,7 +3,7 @@ import { createContext, ContextProvider, UseStateStore } from './createContext';
 import { UseSelector } from '../core/useSelector';
 
 export type UseStateContextSource = {
-  [displayName: string]: any;
+  [partial: string]: any;
 };
 
 /**
@@ -12,11 +12,6 @@ export type UseStateContextSource = {
  * respectively, to prevent unnecessary rendering.
  */
 export const createUseStateContext = <T extends UseStateContextSource>(
-  /**
-   *  Object's value is passed as an argument to useState.
-   *  Also, the object's key is set to the context's displayname.
-   *  *@see* https://reactjs.org/docs/context.html#contextdisplayname
-   */
   contextSource: T
 ): [UseSelector<UseStateStore<T>>, React.FC<ContextProvider<T>>] => {
   const { useGlobalContext, contextProvider } = createContext(contextSource);
